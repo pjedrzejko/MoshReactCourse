fc5d77e2b3b96a994e8e59f51cf5fe6d75d5ce02
Patrick Jedrzejko <patrickjedrzejko@gmail.com>
2025-12-11 18:02:03 -0500

    initial

---
+++ Mosh-React-App/src/Message.tsx
@@ -0,0 +1,16 @@
+
+// PascalCasing
+function Message() {
+    // JSX: Javascript XML
+    const name = 'Mosh';
+
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+
+
+    return <h1>Hello World!</h1>;
+
+}
+
+export default Message;
